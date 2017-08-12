223dab497260c2a59ed68e8732425af955edf5de
Collin Murdock <collinmurdock22@gmail.com>
2017-08-11 23:35:17 -0400

    animated the quote

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(window).on('scroll',function(){
-        var distance = $(this).scrollTop();
-        if(distance >= 380){
-            $(this).off('scroll');
+    var distance = $(this).scrollTop();
+    if(distance >=70){
+    	$(".quote").addClass("quote_active");
+    }
+    if(distance >= 370){
+        $(this).off('scroll');
 
-            $(".past").addClass("past_active");
-            $(".future").addClass("future_active");
-        }
-    });
+        $(".past").addClass("past_active");
+        $(".future").addClass("future_active");
+    }
+});
